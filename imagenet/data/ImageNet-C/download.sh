cf344a3a1bbdeb82b498a2e688d8e7d4caa59c0b
Qin Wang <wang@qin.ee>
2022-03-25 12:10:20 +0100

    Update download.sh

--- imagenet/data/ImageNet-C/download.sh
+++ imagenet/data/ImageNet-C/download.sh
@@ -3,3 +3,9 @@ wget --content-disposition https://zenodo.org/record/2235448/files/digital.tar?d
 wget --content-disposition https://zenodo.org/record/2235448/files/extra.tar?download=1
 wget --content-disposition https://zenodo.org/record/2235448/files/noise.tar?download=1
 wget --content-disposition https://zenodo.org/record/2235448/files/weather.tar?download=1
+
+tar -zxvf blur.tar
+tar -zxvf digital.tar
+tar -zxvf extra.tar
+tar -zxvf noise.tar
+tar -zxvf weather.tar
